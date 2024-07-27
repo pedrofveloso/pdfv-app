73ec6a08a4e1367d5cb65053d211429fc212e63a
Pedro Veloso <pedrofveloso@gmail.com>
2024-07-27 18:01:55 -0300

    create xcode project

---
+++ pdfv-app/AppEntry.swift
@@ -0,0 +1,17 @@
+//
+//  AppEntry.swift
+//  pdfv-app
+//
+//  Created by Pedro Veloso on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AppEntry: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
